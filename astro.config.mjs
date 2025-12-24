238ac089d55effcdd225e98ade95a91d6a237e5c
chaoheng <wb_chaoheng@meituan.com>
2025-12-24 12:02:35 +0800

    thrid_push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	// 如果部署在 GitHub Pages 子路径下，取消注释并设置你的仓库名
 	// base: '/your-repo-name',
-	// site: 'https://your-username.github.io',
+	// site: 'https://chaoheng1000-alt.github.io',
 	
 	// 输出模式：静态站点
 	output: 'static',
